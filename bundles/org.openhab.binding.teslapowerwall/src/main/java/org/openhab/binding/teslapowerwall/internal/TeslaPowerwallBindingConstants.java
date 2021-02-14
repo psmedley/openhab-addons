d04d103340f74b5178bb5d470c943ca2b49f0a0b
Jared <39014663+baku104788@users.noreply.github.com>
2021-02-14 08:38:20 -0600

    Clean up grid services addition

--- bundles/org.openhab.binding.teslapowerwall/src/main/java/org/openhab/binding/teslapowerwall/internal/TeslaPowerwallBindingConstants.java
+++ bundles/org.openhab.binding.teslapowerwall/src/main/java/org/openhab/binding/teslapowerwall/internal/TeslaPowerwallBindingConstants.java
@@ -34,7 +34,7 @@ public class TeslaPowerwallBindingConstants {
 
     // List of all Channel ids
     public static final String CHANNEL_TESLAPOWERWALL_GRIDSTATUS = "gridstatus";
-    public static final String CHANNEL_TESLAPOWERWALL_GRIDSERVICESACTIVE = "gridservicesactive";
+    public static final String CHANNEL_TESLAPOWERWALL_GRIDSERVICES = "gridservices";
     public static final String CHANNEL_TESLAPOWERWALL_BATTERYSOE = "batterysoe";
     public static final String CHANNEL_TESLAPOWERWALL_MODE = "mode";
     public static final String CHANNEL_TESLAPOWERWALL_RESERVE = "reserve";
